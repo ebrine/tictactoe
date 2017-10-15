f2a56b9ae544725a2b880d1c9cf0100751c220be
Eliza Brine <email": null>
2017-10-14 18:50:14 -0700

    adds methods to update board and tests for various inputs

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -65,6 +65,52 @@ describe "app controller" do
         get '/?board=+++++++++'
         expect(last_response.body).to eq 'o        '
       end
+      describe "blocking x's winning moves" do
+        it "returns board with x horizontal winning move blocked" do
+          get '/?board=o++xx+o+x'
+          expect(last_response.body).to eq 'o  xxoo x'
+        end
+        it "returns board with x vertical winning move blocked" do
+          get '/?board=oxo+x+++x'
+          expect(last_response.body).to eq 'oxo x  ox'
+        end
+        it "returns board with x diagonal winning move blocked" do
+          get '/?board=++ooxox+x'
+          expect(last_response.body).to eq 'o ooxox x'
+        end
+      end
+      describe "completes o's winning moves" do
+        it "returns board with o vertical winning move complete" do
+          get '/?board=o+++x+o+x'
+          expect(last_response.body).to eq 'o  ox o x'
+        end
+        it "returns board with o diagonal winning move complete" do
+          get '/?board=ox+x++x+o'
+          expect(last_response.body).to eq 'ox xo x o'
+        end
+        it "returns board with o horizontal winning move complete" do
+          get '/?board=oo+x++x+x'
+          expect(last_response.body).to eq 'ooox  x x'
+        end
+        it "returns board with additional o" do
+          get '/?board=x+xooxoxo'
+          expect(last_response.body).to eq 'xoxooxoxo'
+        end
+      end
+      describe "when no winning move or blockable move" do
+        it "returns board with additional o on corner if possible" do
+          get '/?board=++++o+x++'
+          expect(last_response.body).to eq 'o   o x  '
+        end
+        it "returns board with additional o anywhere if corner not possible" do
+          get '/?board=x+oooxxxo'
+          expect(last_response.body).to eq 'xooooxxxo'
+        end
+        it "returns board with additional o anywhere if corner not possible" do
+          get '/?board=x+oo+xx+o'
+          expect(last_response.body).to eq "xooo xx o"
+        end
+      end
     end
 
   end
