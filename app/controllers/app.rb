e2c625a41ac96d11d172b60c2471e4062853b0e5
Eliza Brine <elizabrine@gmail.com>
2017-12-08 11:38:51 -0800

    Route returns empty string if no board param is passed"

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,10 +1,14 @@
 require_relative 'board'
 
 get '/' do
-  board = Board.new(params[:board])
-  if board.valid_board? == false
-    status 400
+  if params[:board]
+    board = Board.new(params[:board])
+    if board.valid_board? == false
+      status 400
+    else
+      board.return_move
+    end
   else
-    board.return_move
+    return ''
   end
 end
