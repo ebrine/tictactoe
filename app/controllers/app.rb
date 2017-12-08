d7f3f42a786ee3d49ea5246df7f34b0976491372
Eliza Brine <elizabrine@gmail.com>
2017-12-08 13:36:04 -0800

    Update app.rb

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,5 +1,12 @@
 require_relative 'board'
 
+  options "*" do
+    response.headers["Allow"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
+  end
+
 get '/' do
   if params[:board]
     board = Board.new(params[:board])
